3d468e74a605a7203df5027682c4088c3277cce6
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-04-02 23:41:18 +0200

    minor tweaks

--- scripts/test-chat-completions.sh
+++ scripts/test-chat-completions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OPENAI_API_BASE=${OPENAI_API_BASE:-http://localhost:8686/v1}
-OPENAI_API_KEY=${OPENAI_API_KEY:-duck123}
+OPENAI_API_KEY=${OPENAI_API_KEY:-sk-empty}
 query_chat() {
     logfile="/tmp/$(echo $1 | tr -d '/').log"
     if [ -e "$logfile" ]; then
@@ -12,6 +12,10 @@ query_chat() {
          -d '{"model": "'"$1"'", "messages": [{"role": "user", "content": "'"$2"'"}]}' | tee $logfile | jq '.choices[0].message.content'
     echo "Full log found in: $logfile"
 }
+query_chat llamacpp-Ling-Coder-lite "Answer only with the missing word: The capital of the Netherlands is"
+exit
+query_chat vllm-SmolLM2-1.7B-Instruct "Answer only with the missing word: The capital of Poland is"
+query_chat llamacpp-gemma-3-4b-it "Answer only with the missing word: The capital of Belgium is"
 query_chat llamacpp-Qwen2.5-Coder-32B-Instruct "Answer only with the missing word: The capital of Sweden is"
 query_chat llamacpp-QwQ-32B "Answer only with the missing word: The capital of Germany is"
 query_chat llamacpp-gemma-3-27b-it "Answer only with the missing word: The capital of Norway is"
@@ -19,5 +23,5 @@ query_chat llamacpp-Mistral-Small-3.1-24B-Instruct-2503 "Answer only with the mi
 
 # query_chat exllamav2-Qwen2.5-Coder-14B-Instruct "Answer only with the missing word: The capital of Iceland is"
 
-# query_chat vllm-SmolLM2-1.7B-Instruct "Answer only with the missing word: The capital of Poland is"
+
 # query_chat exllamav2-QwQ-32B "Answer only with the missing word: The capital of Denmark is"
