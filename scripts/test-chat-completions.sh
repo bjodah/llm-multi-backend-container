aeb8db8a1709f240364567c61c0bd28e9e690229
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-05-25 15:11:49 +0200

    updates

--- scripts/test-chat-completions.sh
+++ scripts/test-chat-completions.sh
@@ -13,13 +13,14 @@ query_chat() {
     echo "Full log found in: $logfile"
 }
 if [ $# -eq 0 ]; then
-    query_chat exllamav2-gemma-3-27b "What't the captial of Sweden?"
+    query_chat llamacpp-Qwen3-1.7B "What's the captial of Scandinavia? /no_think"
     exit
+    query_chat llamacpp-devstral-small-2505 "Answer only with the missing word: The capital of Norway is"
+    query_chat exllamav2-gemma-3-27b "What't the captial of Sweden?"
     query_chat llamacpp-gemma-3-27b "Answer only with the missing word: The capital of Norway is"
     query_chat llamacpp-Phi-4 "What't the captial of Sweden?"
     query_chat llamacpp-gemma-3-1b"What't the captial of Sweden?"
     query_chat llamacpp-Qwen3-30B-A3B "/no_think Answer only with the missing word: The capital of Greece is"
-    query_chat llamacpp-Qwen3-1.7B "/no_think What't the captial of Sweden?"
     query_chat llamacpp-Qwen3-32B "/nothink Answer only with the missing word: The capital of Italy is"
     query_chat llamacpp-Qwen3-30B-A3B-Q6_K "/nothink Answer only with the missing word: The capital of Greece is"
     for quant in 30B-A3B 0.6B 1.7B 4B 4B-128K 8B 14B 32B; do
